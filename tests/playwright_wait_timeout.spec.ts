41bb01a6c35230b1758ae4c2ba68c6c1dcb9de9f
veena-n <veenanagaraj1906@gmail.com>
2026-08-31 21:26:11 +0200

    added annotation

--- tests/playwright_wait_timeout.spec.ts
+++ tests/playwright_wait_timeout.spec.ts
@@ -2,6 +2,8 @@ import {test} from '@playwright/test'
 
 test.describe("verify the timeout scenario" , () => {
     test("navigation timeout", async ({page}) => {
+        //test timout
+        test.setTimeout(3_000)
 
         await page.goto("https://sqatools.in/dummy-booking-website/", {timeout : 5_000}) //navigation timeout
         //also we can add the navigation timeout in config file
